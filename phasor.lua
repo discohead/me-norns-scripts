87931ad51184130c46544f10172cea98c1630958
Jared McFarland <jaredcolin@gmail.com>
2021-09-04 16:57:57 -0700

    phasor tweaks

--- phasor.lua
+++ phasor.lua
@@ -2,7 +2,7 @@ local PHI = {}
 
 function PHI.new(division, zero)
     local phi = {
-        division = division or 16,
+        division = division or 4,
         zero = zero or clock.get_beats(),
     }
     setmetatable(phi, PHI)
@@ -17,9 +17,15 @@ function PHI:get(at_beats, division)
     return util.linlin(0, division, 0, 1, at_beats)
 end
 
+function PHI:reset()
+    self.zero = clock.get_beats()
+end
+
 PHI.__call = function(self, ...)
     return (self == PHI) and PHI.new(...) or PHI.get(self, ...)
 end
+
+PHI.metaix = {reset = PHI.reset}
 
 setmetatable(PHI, PHI)
 
