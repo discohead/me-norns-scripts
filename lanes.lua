63293b290a8524806884be72a62d456b882b8452
Jared McFarland <jaredcolin@gmail.com>
2021-09-04 16:06:29 -0700

    some lanes tweaks

--- lanes.lua
+++ lanes.lua
@@ -6,13 +6,13 @@ local L = {}
 -- @tparam table t table containing breakpoints that define the return value at given number of beats
 -- ex: l { {0, 0}, {16, 100}, {32, 0}}
 -- each point can have an optional 3rd element that defines the curve to the next point
--- ex: l { {0, 0, curves.ramp{}}, {16, 100, curves.ease_in{}}, {32, 0, curves.ease_out{}}}}
+-- ex: l { {0, 0, curves.ramp()}, {16, 100, curves.ease_in()}, {32, 0, curves.ease_out()}}}
 -- see the curves library for more info
 -- @tparam[opt] boolean loop wether or not the lane should loop, default: false
 -- @treturn table new Lane
-function L.new(t, loop)
+function L.new(t, loop, callback)
     -- wrap a table in a lane with defaults
-    local l = {data = t, startbeats = clock.get_beats(), loop = loop}
+    local l = {data = t, startbeats = clock.get_beats(), loop = loop, ix=1, callback=callback}
     setmetatable(l, L)
     return l
 end
@@ -59,35 +59,30 @@ end
 -- @treturn number value at specified beats
 function L.interpolate(self, at_beats)
     at_beats = at_beats or clock.get_beats() - self.startbeats
-    local start_level, end_level, start_time, end_time
-    local curve_func
-    local final_point = false
-    for ix, point in ipairs(self.data) do
-        local time = point[1]
-        local level = point[2]
-        if time <= at_beats then
-            start_time = time
-            start_level = level
-            curve_func = point[3] -- will default to linear below
-            if ix == #self.data then
-                if self.loop then
-                    self.startbeats = clock.get_beats()
-                    end_time = self.data[1][1]
-                    end_level = self.data[1][2]
-                    break
-                else
-                    return level
-                end
-            end
-        else
-            end_time = time
-            end_level = level
-            break
+    local start_time, start_level, start_curve = table.unpack(self.data[self.ix])
+    local next_ix = (self.ix % #self.data) + 1
+    local target_time, target_level, target_curve = table.unpack(self.data[next_ix])
+    if at_beats >= target_time then
+        start_time, start_level, start_curve = target_time, target_level, target_curve
+        self.ix = next_ix
+        next_ix = (self.ix % #self.data) + 1
+        target_time, target_level = table.unpack(self.data[next_ix])
+        if self.callback then
+            self.callback(start_time, self)
         end
     end
-    local phase = util.linlin(start_time, end_time, 0, 1, at_beats)
-    local y = curve_func and curve_func(phase) or phase -- default to linear
-    return util.linlin(0, 1, start_level, end_level, y)
+
+    if self.ix == #self.data then
+        if self.loop then
+            self.startbeats = clock.get_beats()
+        else
+            return start_level
+        end
+    end
+
+    local phase = util.linlin(start_time, target_time, 0, 1, at_beats)
+    local y = start_curve and start_curve(phase) or phase -- default to linear
+    return util.linlin(0, 1, start_level, target_level, y)
 end
 
 --- metamethods
